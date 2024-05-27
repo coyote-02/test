fdffbb3166a2f5c1475f56ac512eeafe7b316615
futo101 <futo101@icloud.com>
2024-05-27 15:15:58 +0900

    複数の障害物の作成、当たり判定の作成、その反映

---
+++ obstacle/buf-item.rb
@@ -0,0 +1,21 @@
+class Heal < Sprite
+    attr_accessor :status
+    def initialize(x, y, image)
+        @status = {
+            damage: rand(1..70),
+            slow: 2
+        }
+        super(x, y, image)
+    end
+    def update
+        self.x += 1
+    end
+    
+    def hit(obj)
+        if obj.is_a?(Player)
+            puts "Hit Healed: #{self.status[:damage]}"
+            obj.shot(self)
+            self.vanish
+        end
+    end
+end
